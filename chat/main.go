5981ecf5354521717b5527e11bc0258a2192c90c
Takuya Shigemoto <shigemoto.takuya@gmail.com>
2017-10-22 19:16:25 +0900

    ポート番号の可変対応.

--- chat/main.go
+++ chat/main.go
@@ -6,6 +6,7 @@ import (
 	"sync"
 	"html/template"
 	"path/filepath"
+	"flag"
 )
 
 type templateHandler struct {
@@ -20,17 +21,24 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
 	r := newRoom()
+
 	// ルート.
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
+
 	// チャットルームを開始する.
 	go r.run()
+
 	// Webサーバーを開始する.
+	log.Println("Webサーバを開始します。ポート: ", *addr)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}

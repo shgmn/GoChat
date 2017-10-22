19221d6000367125620c964a84a5b4a45e189341
Takuya Shigemoto <shigemoto.takuya@gmail.com>
2017-10-22 18:22:05 +0900

    とりあえず写経.

--- chat/client.go
+++ chat/client.go
@@ -4,9 +4,13 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// チャットを行っている1人のユーザを表す.
 type client struct {
+	// このクライアントのためのWebSocket.
 	socket	*websocket.Conn
+	// メッセージが送られるチャネル.
 	send	chan []byte
+	// このクライアントが参加しているチャットルーム.
 	room	*room
 }
 
--- chat/main.go
+++ chat/main.go
@@ -11,9 +11,11 @@ import (
 type templateHandler struct {
 	once		sync.Once
 	filename	string
+	// 1つのテンプレートを表す.
 	templ		*template.Template
 }
 
+// HTTPリクエストを処理する.
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
@@ -22,8 +24,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
+	// ルート.
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-
+	http.Handle("/room", r)
+	// チャットルームを開始する.
+	go r.run()
+	// Webサーバーを開始する.
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
